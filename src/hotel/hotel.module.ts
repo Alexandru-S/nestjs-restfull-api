a768a83d1d173a699c55da7aebcf33b39deeb367
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-01-02 11:57:51 +0100

    chore: imported prisma in routes

--- src/hotel/hotel.module.ts
+++ src/hotel/hotel.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HotelService } from './hotel.service';
 import { HotelController } from './hotel.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [HotelController],
   providers: [HotelService],
 })
